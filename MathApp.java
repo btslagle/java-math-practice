701db33fe63498b401b19e0cd75d465cadad1050
Brad Slagle <btslagle@gmail.com>
2022-05-23 16:28:02 -0500

    update start question 3

--- MathApp.java
+++ MathApp.java
@@ -1,5 +1,35 @@
 public class MathApp {
+
+  static int job1Salary = 51_000;
+  static int job2Salary = 72_000;
+
+  static int carPrice = 40_000;
+  static int truckPrice = 120_000;
+  static double circleRadius = 7.25;
   public static void main(String[] args) {
-    System.out.println("Answer goes here");
+    System.out.println("");
+  }
+  
+  int findLargestSalary(){
+    return Math.max(job1Salary,job2Salary);  
+  }
+
+  int lowestVechiclePrice(){
+    return Math.min(carPrice,truckPrice);
+  }
+  
+  double  areaOfCircle(){
+    return double.class; Math.PI(circleRadius * circleRadius);
   }
 }
+
+//In `MathApp.java`, write methods for the following:
+
+//1. Find and display the largest of two variables named `job1Salary` and `job2Salary` using `Math.max()`. Set the variables to any value you want.
+//2. Find and display the smallest of two variables named `carPrice` and `truckPrice`. Set the variables to any value you want.
+//3. Find and display the area of a circle whose radius is 7.25.
+//4. Find and display the square root the variable `num1` after it is set to `5.0`.
+//5. Find and display the distance between the points (5, 10) and (85, 50).
+//6. Find and display the absolute (positive) value of `num2` after it is set to `-3.8`.
+//7. Find and display a random number between 0 and 1.
+//
